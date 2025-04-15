7ee9e4a7ad9fb8334551f41da62ec6b4640a8385
Chetanghotekar <chetan.ghotekar_24uds@sanjivani.edu.in>
2025-04-15 11:49:07 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,10 +1,1 @@
-Set 3
-Q1. Git - Branching & PR
-1.	git checkout -b search-feature
-2.	git add . && git commit -m "search feature" && git push origin search-feature
-3.	Open PR & assign reviewer
-4.	Merge PR
-Q2. JQL
-a.	created >= "2025-03-01" AND created <= "2025-03-10"
-b.	updated >= -3d
-c.	status CHANGED FROM "In Progress" TO "Testing" AFTER "2025-04-01"updated vs created: updated includes edits
+print("chetan")
